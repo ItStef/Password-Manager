8c485a7885c2f2845d7a40e52fc85fb83eb27316
Stef <stefanlucic123@gmail.com>
2024-02-20 23:23:51 +0100

    Added functionality for saving savingn which account it is for and username.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 import encrypt as ec
 import saving as sv
+import menu as mn
 
 window = tk.Tk()
 window.title("Password Manager")
 window.geometry("1280x720")
 
 sv.saving_account(window)
+
 
 # Start the GUI event loop
 window.mainloop()
--- saving.py
+++ saving.py
@@ -3,23 +3,61 @@ import tkinter as tk
 import encrypt as ec
 
 def saving_account(window: tk.Tk):
-    password_field = tk.Entry(window, show="*")
-    password_field.pack()
 
-    def save_password():
+    account_for_field = tk.Entry(window, text="Account for: ")
+    account_for_field.pack()
+
+    username_field = tk.Entry(window, text="Username: ")
+    username_field.pack()
+
+    password_field = tk.Entry(window, text= "Password: " ,show="*")
+    password_field.pack() 
+    
+    def save_account():
+        account_for = account_for_field.get()
+        username = username_field.get()
         password = password_field.get()
+        
+        account_for = account_for.encode()
+        username = username.encode()
+
         key = ec.get_key()
         ciphertext = ec.encrypt(password,key = key)
         with open("passwords.txt", "wb") as file:
-            file.write(key + b"|KEY-DATA-DEVIDER|" + ciphertext)    #combine key - password pair with devider
-        password_field.delete(0, "end")
-    save_button = tk.Button(window, text="Save", command=save_password)
+            file.write(account_for + b"|FOR-USER-DEVIDER|" + username+ b"|USER-KEY-DEVIDER|" + key + b"|KEY-DATA-DEVIDER|" + ciphertext)    #combine key - password pair with devider
+        #password_field.delete(0, "end")
+
+
+
+    save_button = tk.Button(window, text="Save", command=save_account)
     save_button.pack()
-    
+
     # Create a button to decrypt the password
     def decrypt_password():
         with open("passwords.txt", "rb") as file:
-            key, ciphertext = file.read().split(b"|KEY-DATA-DEVIDER|")  #split key - password pair with devider
+            line = file.read()
+            if b"|KEY-DATA-DEVIDER|" in line:
+                account_for, temp_rest = line.split(b"|FOR-USER-DEVIDER|")
+                username, temp_rest = temp_rest.split(b"|USER-KEY-DEVIDER|")
+                key, ciphertext = temp_rest.split(b"|KEY-DATA-DEVIDER|")
+            else:
+                return "Invalid file format"
+        
+        aacount_for = account_for.decode()
+        username = username.decode()
         password = ec.decrypt(ciphertext,key)
+
+        account_for_label = tk.Label(window, text=account_for.decode())
+        account_for_label.pack()
+        username_label = tk.Label(window, text=username)
+        username_label.pack()
+        password_label = tk.Label(window, text=password)
+        password_label.pack()
+        account_for_label.after(5000, account_for_label.destroy)
+        username_label.after(5000, username_label.destroy)
+        password_label.after(5000, password_label.destroy)
+
+
+
     decrypt_button = tk.Button(window, text="Decrypt", command=decrypt_password)
     decrypt_button.pack()
